6aee7a62a4e9b0111f3ffd9cfe7c3a1b41c45340
Pangratie Andrei <andrpangr@gmail.com>
2018-08-05 19:01:14 +0300

    files

---
+++ cmmp/parse.cpp
@@ -0,0 +1,6 @@
+#include "parse.h"
+
+namespace util
+{
+	
+}
--- cmmp/test.cpp
+++ cmmp/test.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include "cmmp.h"
 #include "test.h"
+#include "json.h"
+#include "parse.h"
 
 int main()
 {
-	std::cout << "this" << std::endl;
+	nlohmann::json data;
+	std::ifstream("data.json") >> data;
+
+	std::cout << "data:" << std::endl;
+	for (auto&& elem : data["data"])
+		std::cout << elem << std::endl;
+
+	std::vector<std::pair<std::string, double>> v(
+		{
+			std::make_pair(std::string("a"), 20.),
+			std::make_pair(std::string("b"), 15.)
+		}
+	);
+
+	std::cout << util::eval_expr<double>("a + b", v) << std::endl;
 	return 0;
 }
